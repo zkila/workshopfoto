d00ea890751a00c76073bfdbd95a92b55e00a5ad
Arkana Bilal Imani <arkana2003@gmail.com>
2026-06-22 10:06:07 +0700

    feat: add css stuff

--- better-bar/astro.config.mjs
+++ better-bar/astro.config.mjs
@@ -3,9 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   }
 });
+
+export const colors = {
+  background: "#0F0F0F",
+  surface: "#181818",
+  text: "#FFFFFF",
+  textMuted: "#A0A0A0",
+  accent: "#D4AF37"
+};
+
---
+++ better-bar/src/global.d.ts
@@ -0,0 +1,5 @@
+// ./src/globals.d.ts
+
+declare module "*.css";
+declare module "@fontsource/*" {}
+declare module "@fontsource-variable/*" {}
